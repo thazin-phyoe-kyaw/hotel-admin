59470c2dac68ec7522104dded69d9e89bcb77780
thazinphyoekyaw <thazinphyoe455@gmail.com>
2026-02-21 18:02:13 +0630

    login

--- proxy.ts
+++ proxy.ts
@@ -1,47 +1,25 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server"; // Type သတ်မှတ်ရန်
 
-// export function proxy(req: NextRequest) {
-//   const token = req.cookies.get("token")?.value;
-//   const { pathname } = req.nextUrl;
-
-//   if (!token) {
-//     if (pathname !== "/login") {
-//       return NextResponse.redirect(new URL("/login", req.url));
-//     }
-//     return NextResponse.next();
-//   }
-
-//   if (pathname === "/" || pathname === "/login") {
-//     return NextResponse.redirect(new URL("/admin/dashboard", req.url));
-//   }
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: ["/", "/login", "/admin/:path*"],
-// };
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
+import type { NextRequest } from "next/server"; 
 export function proxy(req: NextRequest) {
-//   const token = req.cookies.get("token")?.value;
-//   const { pathname } = req.nextUrl;
+  const token = req.cookies.get("token")?.value;
+  const { pathname } = req.nextUrl;
 
-//   // Block admin when not logged in
-//   if (!token && pathname.startsWith("/admin")) {
-//     return NextResponse.redirect(new URL("/login", req.url));
-//   }
+  if (!token) {
+    console.log(token)
+    if (pathname !== "/login") {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+    return NextResponse.next();
+  }
 
-//   // Redirect logged-in users away from login page
-//   if (token && pathname === "/login") {
-//     return NextResponse.redirect(new URL("/admin/dashboard", req.url));
-//   }
+  if (pathname === "/" || pathname === "/login") {
+    return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: ["/admin/:path*", "/login"],
+export const config = {
+  matcher: ["/", "/login", "/admin/:path*"],
 };
