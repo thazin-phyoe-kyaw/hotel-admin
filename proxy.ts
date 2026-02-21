d1cb337ec099a7d30aee1d4fa078e3181260ad27
thazinphyoekyaw <thazinphyoe455@gmail.com>
2026-02-21 17:33:50 +0630

    d

--- proxy.ts
+++ proxy.ts
@@ -22,26 +22,26 @@
 // export const config = {
 //   matcher: ["/", "/login", "/admin/:path*"],
 // };
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export function proxy(req: NextRequest) {
-  const token = req.cookies.get("token")?.value;
-  const { pathname } = req.nextUrl;
+// export function proxy(req: NextRequest) {
+//   const token = req.cookies.get("token")?.value;
+//   const { pathname } = req.nextUrl;
 
-  // Block admin when not logged in
-  if (!token && pathname.startsWith("/admin")) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+//   // Block admin when not logged in
+//   if (!token && pathname.startsWith("/admin")) {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
 
-  // Redirect logged-in users away from login page
-  if (token && pathname === "/login") {
-    return NextResponse.redirect(new URL("/admin/dashboard", req.url));
-  }
+//   // Redirect logged-in users away from login page
+//   if (token && pathname === "/login") {
+//     return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/admin/:path*", "/login"],
-};
+// export const config = {
+//   matcher: ["/admin/:path*", "/login"],
+// };
